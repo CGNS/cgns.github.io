6fee3f36a033b00077534ac2524cb6b062d2b1bd
Mickael PHILIT <mickey.phy@gmail.com>
2025-01-09 07:45:54 +0100

    Improve rendering of filemap boxes containers

--- conf.py
+++ conf.py
@@ -101,7 +101,7 @@ html_static_path = ["_static"]
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
-html_css_files = ["css/custom.css"]
+html_css_files = ["css/custom.css", 'css/filemap.css']
 html_theme_options = {
     "repository_url": "https://github.com/CGNS/cgns-modern.github.io",
     "use_issues_button": True,
